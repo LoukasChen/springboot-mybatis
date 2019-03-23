5a29d8450842f1921316266705d157df33701c6f
csp <jeffcsp@163.com>
2019-03-23 23:04:14 +0800

    删除.idea文件

--- src/main/java/com/csp/config/MyMvcConfig.java
+++ src/main/java/com/csp/config/MyMvcConfig.java
@@ -1,26 +1,26 @@
-//package com.csp.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-///**
-// *
-// 使用WebMvcConfigurerAdapter扩展springmvc功能
-// */
-//@Configuration
-//public class MyMvcConfig extends WebMvcConfigurerAdapter {
-//
-//    @Bean
-//    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
-//        WebMvcConfigurerAdapter webMvcConfigurerAdapter = new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addViewControllers(ViewControllerRegistry registry) {
-//                registry.addViewController("/").setViewName("/login");
-//                registry.addViewController("/login.html").setViewName("/login");
-//            }
-//        };
-//        return webMvcConfigurerAdapter;
-//    }
-//}
+package com.csp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ 使用WebMvcConfigurerAdapter扩展springmvc功能
+ */
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter webMvcConfigurerAdapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("/login");
+                registry.addViewController("/login.html").setViewName("/login");
+            }
+        };
+        return webMvcConfigurerAdapter;
+    }
+}
