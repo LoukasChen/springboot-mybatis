4b4aea89f5dc628536add0b1364e47f6aec44793
csp <jeffcsp@163.com>
2019-03-23 22:43:40 +0800

    添加过滤器

--- src/main/java/com/csp/Filter/WordsFilter.java
+++ src/main/java/com/csp/Filter/WordsFilter.java
@@ -1,49 +1,49 @@
-//package com.csp.Filter;
-//
-//import com.csp.Utils.WordsUtil;
-//
-//import javax.servlet.*;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletRequestWrapper;
-//import java.io.IOException;
-//import java.util.List;
-//
-///**
-// * 过滤敏感词
-// */
-//public class WordsFilter implements Filter {
-//
-//    @Override
-//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        servletRequest.setCharacterEncoding("utf-8");
-//        WordsRequest request = new WordsRequest((HttpServletRequest) servletRequest);
-//        filterChain.doFilter(request,servletResponse);
-//    }
-//
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//
-//    }
-//
-//    @Override
-//    public void destroy() {
-//
-//    }
-//
-//    class WordsRequest extends HttpServletRequestWrapper{
-//
-//        public WordsRequest(HttpServletRequest request) {
-//            super(request);
-//        }
-//
-//        @Override
-//        public String getParameter(String name) {
-//            String input = super.getParameter(name);
-//            List<String> list = WordsUtil.getWords();
-//            for(String word : list){
-//                input = input.replaceAll(word, "*");
-//            }
-//            return input;
-//        }
-//    }
-//}
+package com.csp.Filter;
+
+import com.csp.Utils.WordsUtil;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 过滤敏感词
+ */
+public class WordsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("utf-8");
+        WordsRequest request = new WordsRequest((HttpServletRequest) servletRequest);
+        filterChain.doFilter(request,servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    class WordsRequest extends HttpServletRequestWrapper{
+
+        public WordsRequest(HttpServletRequest request) {
+            super(request);
+        }
+
+        @Override
+        public String getParameter(String name) {
+            String input = super.getParameter(name);
+            List<String> list = WordsUtil.getWords();
+            for(String word : list){
+                input = input.replaceAll(word, "*");
+            }
+            return input;
+        }
+    }
+}
--- src/main/java/com/csp/config/FilterConfig.java
+++ src/main/java/com/csp/config/FilterConfig.java
@@ -1,18 +1,18 @@
-//package com.csp.config;
-//
-//import com.csp.Filter.WordsFilter;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class FilterConfig {
-//
-// @Bean
-// public FilterRegistrationBean wordsFilterRegistrationBean(){
-//     FilterRegistrationBean bean = new FilterRegistrationBean();
-//     bean.setFilter(new WordsFilter());
-//     bean.addUrlPatterns("/*");
-//     return bean;
-// }
-//}
+package com.csp.config;
+
+import com.csp.Filter.WordsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+ @Bean
+ public FilterRegistrationBean wordsFilterRegistrationBean(){
+     FilterRegistrationBean bean = new FilterRegistrationBean();
+     bean.setFilter(new WordsFilter());
+     bean.addUrlPatterns("/*");
+     return bean;
+ }
+}
